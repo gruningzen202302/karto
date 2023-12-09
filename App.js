cecb047be2347d859cc8f6136b8ea0b081eddb32
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-12-08 22:40:18 -0300

    📦 NEW: add Finite State Machine implementation for shopping orders

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
+import { InputAccessoryView, StyleSheet, Text, View } from "react-native"
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import { useCallback } from "react"
+
+//tutorial video Javascript Finite State Machine
+//https://youtu.be/5WrDgEPZeuI
 
 export default function App() {
   const copyleftSymbol = String.fromCodePoint(0x1f12f)
@@ -14,6 +17,78 @@ export default function App() {
     extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
     semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
   })
+
+  class FiniteStateMachine {
+    constrctor() {
+      this.states = {
+        currentState: null, //"Pending", //null , //q0
+        finalState: "Complete", //q1
+        inicialState: "Pending", //q0
+      }
+      this.transition = {
+        delta: (input, state = this.states.inicialState) => {
+          if (input.length === 0) return
+          //let currentInput = input.shift()//TODO implement more efficient approach if it is easy to convert to othjer languages
+          let currentInput = input[0]
+          input = input.slice(1) //, input.length)
+          let mapping = this.transition.mapping(state)
+          this.states.currentState = mapping.get(currentInput)
+          this.transition.delta(input, this.states.currentState)
+        },
+        mapping: () => {
+          let stateMap = new Map()
+          stateMap.set(
+            "Pending",
+            new Map([
+              ["0", "Pending"],
+              ["1", "On Hold"],
+              ["2", "Processing"],
+              ["3", "Cancelled"],
+            ])
+          )
+          stateMap.set(
+            "On Hold",
+            new Map([
+              ["0", "On Hold"],
+              ["1", "Processing"],
+              ["2", "Cancelled"],
+              ["3", "Complete"],
+              ["4", "Closed"],
+              ["5", "Payment Review"],
+              ["6", "Pending Payment"],
+            ])
+          )
+          stateMap.set(
+            "Processing",
+            new Map([
+              ["0", "On Hold"],
+              ["1", "Cancelled"],
+              ["2", "Complete"],
+              ["4", "Payment Review"],
+              ["5", "Pending Payment"],
+            ])
+          )
+
+          return stateMap.get(state)
+        },
+      }
+    }
+
+    evaluate(input) {
+      this.transition.delta(input)
+      let result
+      this.states.currentState === this.states.finalState
+        ? (result = true)
+        : (result = false)
+      return result
+    }
+  }
+
+  let shopOrderStateMachine = new FiniteStateMachine()
+  let input = ["Pending", "Cancelled"]
+  //let result = shopOrderStateMachine.evaluate(input)
+  console.warn("shopOrderStateMachine")
+  //console.warn(result)
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
